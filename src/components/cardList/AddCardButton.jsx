d123a0944ff2e3987c0cca7d732f57bba9014722
bywdsg <liuyao928@tom.com>
2019-12-16 02:26:17 +1000

    stylelint fix

--- src/components/cardList/AddCardButton.jsx
+++ src/components/cardList/AddCardButton.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function AddCardButton() {
+function AddCardButton () {
   return (
     <li className='col-12 col-sm-6 col-lg-3'>
       <div className='single-card single-card--new'>
-       +                     
-      </div> 
+        +
+      </div>
       <div className='single-card--hover'>
         <Link
           to='/add'
         >
           <button
             className='single-card__button single-card__button--add'
-            >Add</button>
+          >Add</button>
         </Link>
       </div>
     </li>
